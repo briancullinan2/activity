1569db130286a623572cb492015c1a1fc40cd14c
Brian Cullinan <112788814+briancullinan2@users.noreply.github.com>
2022-09-04 20:33:06 -0700

    ready to look for bookmarks file

--- bookmarks.js
+++ bookmarks.js
@@ -2,6 +2,36 @@
 const {decryptFile} = require('./utilities.js')
 
 
+const fs = require('fs')
+const path = require('path')
+const HOMEPATH = process.env.HOME || process.env.HOMEPATH || process.env.USERPROFILE
+
+
+function findBookmarksFile () {
+    let workingPaths = []
+    let settingsPath
+
+    if (os.platform == 'win32') {
+        settingsPath = path.join(HOMEPATH, 'AppData\/LocalStorage')
+      } else {
+        if (os.platform == 'darwin') {
+            settingsPath = path.join(HOMEPATH, 'Library\/Application\ Support')
+        } else {
+            settingsPath = path.join(HOMEPATH, '.config')
+        }
+    }
+
+    workingPaths.push(path.join(settingsPath, 'Google\/Chrome\/Default/Bookmarks'))
+    workingPaths.push(path.join(settingsPath, 'BraveSoftware\/Brave-Browser\/Default\/Bookmarks'))
+    
+    for(let i = 0; i < workingPaths.length; i++) {
+        if(fs.existsSync(workingPaths[i])) {
+            return workingPaths[i]
+        }
+    }
+}
+
+
 // TODO: decrypt uploaded file
 
 // TODO: parse bookmarks
--- utilities.js
+++ utilities.js
@@ -5,9 +5,41 @@
 // this is a workflow thing, a minimal example
 
 
+// SOURCE: https://stackoverflow.com/questions/18279141/javascript-string-encryption-and-decryption
+
+function crypt (salt, text) {
+    const textToChars = (text) => text.split("").map((c) => c.charCodeAt(0));
+    const byteHex = (n) => ("0" + Number(n).toString(16)).substr(-2);
+    const applySaltToChar = (code) => textToChars(salt).reduce((a, b) => a ^ b, code);
+  
+    return text
+      .split("")
+      .map(textToChars)
+      .map(applySaltToChar)
+      .map(byteHex)
+      .join("");
+}
+  
+function decrypt (salt, encoded) {
+    const textToChars = (text) => text.split("").map((c) => c.charCodeAt(0));
+    const applySaltToChar = (code) => textToChars(salt).reduce((a, b) => a ^ b, code);
+    return encoded
+      .match(/.{1,2}/g)
+      .map((hex) => parseInt(hex, 16))
+      .map(applySaltToChar)
+      .map((charCode) => String.fromCharCode(charCode))
+      .join("");
+}
+
 // TODO: encrypt a file
+function encryptFile(file) {
+
+}
 
 // TODO: decrypt a file
+function decryptFile(file) {
+    
+}
 
 module.exports = {
     decryptFile,
