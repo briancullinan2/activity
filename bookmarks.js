4ebab53f3daba0d852e310dd6e3706f6b88e92ec
megamind <megamindbrian@gmail.com>
2022-09-04 22:47:40 -0700

    nearly to html building, then publish with CI, then update with reverse proxy

--- bookmarks.js
+++ bookmarks.js
@@ -1,11 +1,12 @@
 
-const {decryptFile} = require('./utilities.js')
-
-
 const fs = require('fs')
 const path = require('path')
-const HOMEPATH = process.env.HOME || process.env.HOMEPATH || process.env.USERPROFILE
+const JSONStream = require('JSONStream')
+const {Readable} = require('stream')
+const {decryptFile, chromeDtToDate} = require('./utilities.js')
 
+
+const HOMEPATH = process.env.HOME || process.env.HOMEPATH || process.env.USERPROFILE
 
 function findBookmarksFile () {
     let workingPaths = []
@@ -21,8 +22,8 @@ function findBookmarksFile () {
         }
     }
 
-    workingPaths.push(path.join(settingsPath, 'Google\/Chrome\/Default/Bookmarks'))
     workingPaths.push(path.join(settingsPath, 'BraveSoftware\/Brave-Browser\/Default\/Bookmarks'))
+    workingPaths.push(path.join(settingsPath, 'Google\/Chrome\/Default/Bookmarks'))
     
     for(let i = 0; i < workingPaths.length; i++) {
         if(fs.existsSync(workingPaths[i])) {
@@ -32,11 +33,67 @@ function findBookmarksFile () {
 }
 
 
-// TODO: decrypt uploaded file
+function decryptBookmarks() {
+    const bookmarksFile = findBookmarksFile()
+    const bookmarksData = fs.readFileSync(bookmarksFile).toString('utf-8')
+    let decryptedBookmarks
+    if(bookmarksData[0] == '{') {
+        decryptedBookmarks = bookmarksData
+    } else {
+        decryptedBookmarks = decryptFile(bookmarksData)
+    }
+    
+    return decryptedBookmarks
+}
 
-// TODO: parse bookmarks
+function recursiveGroup(root, obj, book) {
+    if(typeof book.children != 'undefined') {
+        let groupName = book.name
+        let recursiveFunc = recursiveGroup.bind(null, (root.includes('Other Bookmarks') ? '' : (root + '/')) + book.name)
+        let children = book.children.reduce(recursiveFunc, {})
+        if(typeof obj[groupName] == 'undefined') {
+            obj[groupName] = {}
+        }
+        Object.assign( obj[groupName], children)
+    } else {
+        book.folder = root
+        book.time_usec = parseInt(book.date_added + ''),
+        book.date = chromeDtToDate(book.time_usec)
+        obj[book.guid] = book
+    }
+    return obj
+}
+
+function parseBookmarks() {
+    let decryptedBookmarks = decryptBookmarks()
+    let root = JSON.parse(decryptedBookmarks).roots.bookmark_bar.children
+    let bookmarks = root.reduce(recursiveGroup.bind(null, ''), {})
+
+    // from this verified structure, list newest additions
+    let flattened = Object.values(bookmarks).reduce(function flatten(list, obj) {
+        if(typeof obj.folder != 'undefined') {
+            list.push(obj)
+        } else {
+            let children = Object.values(obj).reduce(flatten, [])
+            for(let i = 0; i < children.length; i++) {
+                list.push(children[i])
+            }
+        }
+        return list
+    }, [])
+
+    return flattened
+}
 
 // TODO: make an html page out of categories
+function outputBookmarks() {
+    let bookmarks = parseBookmarks()
+    let recentlyAdded = bookmarks.filter(book => book.date.getTime() > Date.now() - 24 * 60 * 60 * 1000)
+    console.log(recentlyAdded)
+}
 
 // TODO: commit to a seperate branch using GitHub Actions CI
 
+module.exports = {
+    outputBookmarks
+}
--- utilities.js
+++ utilities.js
@@ -3,14 +3,19 @@
 //   the option is there to prevent snooping on everything
 
 // this is a workflow thing, a minimal example
+const fs = require('fs')
+const path = require('path')
 
+const HOMEPATH = process.env.HOME || process.env.HOMEPATH || process.env.USERPROFILE
+const PASSWORD = process.env.ENCRYPTION_SALT 
+    || fs.readFileSync(path.join(HOMEPATH, '.credentials\/bookmarks.txt'))
 
 // SOURCE: https://stackoverflow.com/questions/18279141/javascript-string-encryption-and-decryption
 
 function crypt (salt, text) {
     const textToChars = (text) => text.split("").map((c) => c.charCodeAt(0));
     const byteHex = (n) => ("0" + Number(n).toString(16)).substr(-2);
-    const applySaltToChar = (code) => textToChars(salt).reduce((a, b) => a ^ b, code);
+    const applySaltToChar = (code) => textToChars(salt).reduce((a, b) => a ^ b, code)
   
     return text
       .split("")
@@ -22,7 +27,7 @@ function crypt (salt, text) {
   
 function decrypt (salt, encoded) {
     const textToChars = (text) => text.split("").map((c) => c.charCodeAt(0));
-    const applySaltToChar = (code) => textToChars(salt).reduce((a, b) => a ^ b, code);
+    const applySaltToChar = (code) => textToChars(salt).reduce((a, b) => a ^ b, code)
     return encoded
       .match(/.{1,2}/g)
       .map((hex) => parseInt(hex, 16))
@@ -33,15 +38,23 @@ function decrypt (salt, encoded) {
 
 // TODO: encrypt a file
 function encryptFile(file) {
-
+    return crypt(PASSWORD, file)
 }
 
 // TODO: decrypt a file
 function decryptFile(file) {
-    
+    return decrypt(PASSWORD, file)
+}
+
+function chromeDtToDate(st_dt) {
+    var microseconds = parseInt(st_dt, 10);
+    var millis = microseconds / 1000;
+    var past = new Date(1601, 0, 1).getTime();
+    return new Date(past + millis);
 }
 
 module.exports = {
     decryptFile,
     encryptFile,
+    chromeDtToDate,
 }
