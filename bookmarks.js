02abf51984e77b07acb2e0900963f8ea3b5708e8
Brian Cullinan <megamindbrian@gmail.com>
2022-10-06 09:48:53 -0700

    "auto-update"

--- bookmarks.js
+++ bookmarks.js
@@ -65,6 +65,7 @@ function recursiveGroup(root, obj, book) {
 
 function parseBookmarks() {
 	let decryptedBookmarks = decryptBookmarks()
+	console.log(decryptBookmarks)
 	let root = JSON.parse(decryptedBookmarks).roots.bookmark_bar.children
 	let bookmarks = root.reduce(recursiveGroup.bind(null, ''), {})
 
