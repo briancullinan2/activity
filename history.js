246dfe58eac446480862235ba2314445efe062d3
Brian Cullinan <megamindbrian@gmail.com>
2022-10-08 13:44:35 -0700

    "auto-update"

--- history.js
+++ history.js
@@ -50,7 +50,7 @@ function getHistory() {
 
 	// reverse of chromeDtToDate
 	const todayOffset = (Date.now() - BASE_DATE) * 1000
-	console.log(BASE_DATE)
+	console.log(UNIX_EPOCH + BASE_DATE)
 	const results = db.prepare('SELECT * FROM urls WHERE last_visit_time > ?').all(todayOffset)
 	//console.log(results)
 	return results
