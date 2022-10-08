441c3b43945a7a9f4f1357dd3aa4972af7c26d84
Brian Cullinan <megamindbrian@gmail.com>
2022-10-08 13:40:19 -0700

    "auto-update"

--- history.js
+++ history.js
@@ -9,8 +9,8 @@ const sqlite3 = require('better-sqlite3');
 
 const HOMEPATH = process.env.HOME || process.env.HOMEPATH || process.env.USERPROFILE
 
-const BASE_DATE = new Date(1601, 0, 1, 0, 0, 0, 0).getTime() // - 60 * 30 * 1000
-const UNIX_EPOCH = new Date(1970, 0, 1, 0, 0, 0, 0).getTime() // - 60 * 30 * 1000
+const BASE_DATE = new Date(1601, 0, 0, 0, 0, 0, 0).getTime() // - 60 * 30 * 1000
+const UNIX_EPOCH = new Date(1970, 0, 0, 0, 0, 0, 0).getTime() // - 60 * 30 * 1000
 const TIME_ZONE = (new Date).getTimezoneOffset()
 
 function findHistoryFile() {
