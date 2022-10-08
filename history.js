8bf5b8d3f82e430f377d7522f4a5281ffdcb1f70
Brian Cullinan <megamindbrian@gmail.com>
2022-10-08 13:18:16 -0700

    "auto-update"

--- history.js
+++ history.js
@@ -46,7 +46,7 @@ function getHistory() {
 		readonly: true,
 		fileMustExist: true,
 	})
-	const today = Math.floor(Date.now() / 60 / 60 / 96) * 60 * 60 * 96
+	const today = Math.floor(Date.now() / 60 / 60 / 24 / 4) * 60 * 60 * 24 * 4
 	// reverse of chromeDtToDate
 	const todayOffset = (today - BASE_DATE) * 1000
 
