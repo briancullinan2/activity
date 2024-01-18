60edd1c6699a145d023064cbe82b32939889571f
Brian Cullinan <megamindbrian@gmail.com>
2024-01-17 17:00:37 -0700

    "auto-update"

--- calendar.js
+++ calendar.js
@@ -125,7 +125,7 @@ async function listCalendar() {
 		let eventsList = []
 		for (const event of Object.values(events)) {
 			event.content = (event.summary || '') + '\n' + (event.description || '')
-			event.name = fname.trim().replace(/_.*?\.ics$/, '')
+			event.name = path.basename(fname).trim().replace(/_.*?\.ics$/, '')
 			eventsList[eventsList.length] = event
 		}
 
